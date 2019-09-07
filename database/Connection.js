cbfe9ac8cf73fb8350e253415eb3fe483eafb12d
Maxx730 <max.kinghorn@gmail.com>
2019-09-07 16:15:13 -0400

    :update:

--- database/Connection.js
+++ database/Connection.js
@@ -3,7 +3,8 @@ let pool = mysql.createPool({
     connectionLimit:100,
     host:'localhost',
     user:'root',
-    password:'dRmario43'
+    password:'dRmario43',
+    database:'songshare'
 });
 
 module.exports = pool;
