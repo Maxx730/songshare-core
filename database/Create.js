cafd510ca8e1ec08a26ebc7c1d22096e996788eb
Maxx730 <max.kinghorn@gmail.com>
2019-09-07 16:34:16 -0400

    update

--- database/Create.js
+++ database/Create.js
@@ -5,7 +5,7 @@ function CreationController (DatabaseConntection,ExpressApplication){
     this.app.use('/database/create',(req,res) => {
         this.connection.query('CREATE TABLE IF NOT EXISTS users(_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,username VARCHAR(55) NOT NULL,password VARCHAR(55) NOT NULL,email VARCHAR(55) NOT NULL)',(err,result,fields) => {
             if(!err) {
-                console.log('database created!')
+                console.log(result)
             } else {
                 console.log(err)
             }
