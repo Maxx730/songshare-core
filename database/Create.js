6e89799037d5c318d3cb645865e94277464e213e
Maxx730 <max.kinghorn@gmail.com>
2019-09-07 16:41:31 -0400

    update:

--- database/Create.js
+++ database/Create.js
@@ -5,7 +5,13 @@ function CreationController (DatabaseConntection,ExpressApplication){
     this.app.use('/database/create',(req,res) => {
         this.connection.query('CREATE TABLE IF NOT EXISTS users(_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,username VARCHAR(55) NOT NULL,password VARCHAR(55) NOT NULL,email VARCHAR(55) NOT NULL)',(err,result,fields) => {
             if(!err) {
-                console.log(result)
+                this.connection.query('USER DATABASE songshare;INSERT INTO users(username,password,email) VALUES(`maxx730`,`drmario`,`max.kinghorn@gmail.com`);',(err,result) => {
+                    if (!err) {
+                        console.log(result)
+                    } else {
+                        console.log(err);
+                    }
+                });
             } else {
                 console.log(err)
             }
