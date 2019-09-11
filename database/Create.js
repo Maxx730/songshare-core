97bcc042bda9ad3aa160bc359235970f3d1a0baf
John Kinghorn <john.kinghorn@coxautoinc.com>
2019-09-11 15:41:35 -0400

    update

--- database/Create.js
+++ database/Create.js
@@ -7,7 +7,7 @@ function CreationController (DatabaseConntection,ExpressApplication){
 			if(!err) {
 				this.connection.query('CREATE TABLE IF NOT EXISTS shared(_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,sharer INT NOT NULL,title VARCHAR(100) NOT NULL,artist VARCHAR(100) NOT NULL,art VARCHAR(100) NOT NULL,time_shared TIMESTAMP DEFAULT CURRENT_TIMESTAMP,duration INT,spotify_id VARCHAR(999),youtube_id VARCHAR(999),play_id VARCHAR(999))',(err,result) => {
 					if(!err) {
-						this.connection.query('CREATE TABLE IF NOT EXISTS friends(_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,receiver INT NOT NULL,sender INT NOT NULL,accepted BOOL NOT NULL DEFAULT FALSE)',(err,result) => {
+						this.connection.query('CREATE TABLE IF NOT EXISTS friends(_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,sender INT NOT NULL,receiver INT NOT NULL,accepted BOOL NOT NULL DEFAULT FALSE)',(err,result) => {
 							if(!err) {
 								console.log('DATABASE SUCCESSFULLY CREATED!');
 							} else {
@@ -27,14 +27,14 @@ function CreationController (DatabaseConntection,ExpressApplication){
 	this.app.use('/database/purge',(req,res) => {
 		this.connection.query('DROP TABLE users',(err,result) => {
 			if(!err) {
-				console.log('USERS TABLE DELETED\n');
+				console.log('USERS TABLE DELETED');
 				this.connection.query('DROP TABLE friends',(err,result) => {
 					if(!err) {
-						console.log('FRIENDS TABLE DELETED\n');
+						console.log('FRIENDS TABLE DELETED');
 						this.connection.query('DROP TABLE shared',(err,result) => {
 							if(!err) {
-								console.log('SHARED TABLE DELETED\n');
-								console.log('DATABASE PURGE COMPLETED\n');
+								console.log('SHARED TABLE DELETED');
+								console.log('DATABASE PURGE COMPLETED');
 							} else {
 								console.log(err);
 							}
@@ -55,7 +55,14 @@ function CreationController (DatabaseConntection,ExpressApplication){
             if(!err) {
                 this.connection.query('INSERT INTO users(username,password,email) VALUES("maxx730","drmario","max.kinghorn@gmail.com");INSERT INTO users(username,password,email) VALUES("tolm640","drmario","tolm.lamar@gmail.com");INSERT INTO users(username,password,email) VALUES("ethan54","drmario","ethan.clokels@gmail.com");',(err,result) => {
                     if(!err) {
-                        console.log('users added');
+						console.log('USER DATA SEEDED');
+                        this.connection.query('INSERT INTO friends(0,1,false);INSERT INTO friends(0,2,true);INSERT INTO friends(1,2,false);',(err,result) => {
+							if(!err) {
+								console.log('FRIEND DATA SEEDED');
+							} else {
+								console.log(err);
+							}
+						});
                     } else {
                         console.log(err);
                     }
