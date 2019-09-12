a1d3d112e909a4a056bbdaa024520b679fab2138
John Kinghorn <john.kinghorn@coxautoinc.com>
2019-09-12 11:45:57 -0400

    update

--- database/Create.js
+++ database/Create.js
@@ -54,7 +54,7 @@ function CreationController (DatabaseConntection,ExpressApplication){
 
     //Deletes all the data in the database and seeds to data for development purposes.
     this.app.use('/database/seed',(req,res) => {
-        this.connection.query('DELETE FROM users',(err,result) => {
+        this.connection.query('DELETE FROM users', async (err,result) => {
             if(!err) {
 				const hashed = await bcrypt.hash('drmario', 10);
                 this.connection.query('INSERT INTO users(username,password,email) VALUES("maxx730","' + hash + '","max.kinghorn@gmail.com"),("tolm640","drmario","tolm.lamar@gmail.com"),("ethan54","drmario","ethan.clokels@gmail.com");',(err,result) => {
