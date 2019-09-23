3685af9bd5477415cc77824e227069f5b30d1c29
John Kinghorn <john.kinghorn@coxautoinc.com>
2019-09-23 10:07:39 -0400

    update

--- controllers/Users.js
+++ controllers/Users.js
@@ -18,7 +18,7 @@ function UserController(DatabaseConnection,ExpressApp){
     this.app.get('/user/:id/friends',(req,res) => {
         res.set('Content-Type','application/json')
 
-        this.connection.query("SELECT * FROM friends INNER JOIN users senders ON friends.sender=senders._id INNER JOIN users receivers ON friends.receiver=friends._id WHERE friends.sender="+req.params.id,(err,result,fields) => {
+        this.connection.query("SELECT * FROM friends INNER JOIN users senders ON friends.sender=senders._id INNER JOIN users receivers ON friends.receiver=friends._id WHERE friends.sender="+req.params.id + " OR friends.receiver=" + req.params.id,(err,result,fields) => {
           if(!err){
             res.json({
               PAYLOAD:result,
