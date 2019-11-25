d601ec816b48b990df08ffd9d1220b7eb8ea28f1
Maxx730 <max.kinghorn@gmail.com>
2019-11-25 09:55:13 -0500

    update

--- controllers/Shares.js
+++ controllers/Shares.js
@@ -84,7 +84,7 @@ function SharesController(DatabaseConnection,ExpressApplication){
     this.app.get('/user/:id/shares',(req,res) => {
       res.set('Content-Type','application/json');
 
-      let query = "SELECT shared._id,shrs.username as sharer,shrs.profile as profile,shared.title,shared.artist,shared.art,shared.spotify_id,(SELECT COUNT(*) FROM likes WHERE likes.track_id=shared._id) AS likes,(SELECT COUNT(*) FROM likes WHERE likes.track_id=shared._id AND user_id=" + req.params.id + ") AS hasLiked,shared.time_shared AS date FROM  shared JOIN(SELECT frns._id,frns.username FROM friends JOIN(SELECT _id,username FROM users WHERE users._id<>" + req.params.id + ") frns ON frns._id=friends.sender OR frns._id=friends.receiver WHERE friends.sender=" + req.params.id + " OR friends.receiver=" + req.params.id + ") shrs ON shared.sharer=shrs._id ORDER BY date DESC ";
+      let query = "SELECT shared._id,shrs.username as sharer,shared.title,shared.artist,shared.art,shared.spotify_id,(SELECT COUNT(*) FROM likes WHERE likes.track_id=shared._id) AS likes,(SELECT COUNT(*) FROM likes WHERE likes.track_id=shared._id AND user_id=" + req.params.id + ") AS hasLiked,shared.time_shared AS date FROM  shared JOIN(SELECT frns._id,frns.username,frns.profile FROM friends JOIN(SELECT _id,username FROM users WHERE users._id<>" + req.params.id + ") frns ON frns._id=friends.sender OR frns._id=friends.receiver WHERE friends.sender=" + req.params.id + " OR friends.receiver=" + req.params.id + ") shrs ON shared.sharer=shrs._id ORDER BY date DESC ";
 
       this.connection.query(query,(err,result,fields) => {
         if(!err){
@@ -95,7 +95,6 @@ function SharesController(DatabaseConnection,ExpressApplication){
           })
           res.end()
         }else{
-          console.error("something went wrong");
           res.json({
             TYPE:"ERROR",
             MESSAGE:"ERROR PULLING USER SHARE STREAM"
