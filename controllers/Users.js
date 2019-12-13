8584a1d74bd9de2ecf9ddbd37eacf5afc52c24db
maxx730 <max.kinghorn@gmail.com>
2019-12-13 12:32:58 -0500

    debugging

--- controllers/Users.js
+++ controllers/Users.js
@@ -7,7 +7,7 @@ this.connection = DatabaseConnection;
 	this.utils = new Utils(this.connection);
 
     this.app.get('/users',async (req,res) => {
-			console.log(req)
+			console.log(req.headers)
 	  res.set('Content-Type','application/json');
 
 	  //Check Credentials
