c654f00e9f913ade96f445c7355d275d8f8d2e84
maxx730 <max.kinghorn@gmail.com>
2020-01-01 15:44:48 -0500

    added credentials check

--- app.js
+++ app.js
@@ -9,21 +9,16 @@ let body = require('body-parser');
 let cors = require('cors');
 var request = require('request');
 app.use(cookieParser());
+app.use(body.urlencoded({extended:true}));
+app.use(body.json());
 app.use(cors());
 
 //Import our different modules so we can create the end point controllers.
-let pool = require('./database/Connection');
-let UserController = require('./controllers/Users');
-let SharesController = require('./controllers/Shares');
-let CreationHandler = require('./database/Create');
-let Authorization = require('./auth/Authorization.js');
-let AuthEndpoint = require('./controllers/Auth.js');
-
-//Initialize our endpoint controllers.
-let Authorize = new Authorization(pool,app);
-let UserControl = new UserController(pool,app);
-let SharesControl = new SharesController(pool,app);
-let Creation = new CreationHandler(pool,app);
-let AuthEndpointService = new AuthEndpoint(pool,app);
+app.post('/test',(req,res) => {
+  res.json({
+    body: req.body
+  })
+  res.end();
+})
 
 module.exports = app;
