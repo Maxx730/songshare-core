ac923fb5b1138d0c279017d54521e6199f69eabd
maxx730 <max.kinghorn@gmail.com>
2020-01-01 15:23:44 -0500

    added credentials check

--- auth/Authorization.js
+++ auth/Authorization.js
@@ -9,16 +9,7 @@ function Authorization(Database,Application) {
 
     //Anytime any connection is made to the backend database,
     //authorize that this user exists within the database.
-    this.app.use(basicAuth({
-        authorizer: (username,password) => {
-            console.log("AUTHORIZING...");
-            //Check the database for a user with the given password.
-            return true;
-        },
-        unauthorizedResponse: (req) => {
-            return "Authorization Credentials Required";
-        }
-    }));
+
 }
 
 module.exports = Authorization;
