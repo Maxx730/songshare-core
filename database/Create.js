270f34f1754374d7150a27b1404e2a2339d30c3b
maxx730 <max.kinghorn@gmail.com>
2020-01-01 17:28:42 -0500

    added credentials check

--- database/Create.js
+++ database/Create.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 function CreationController (DatabaseConntection,ExpressApplication){
     console.log('STARTING CREATION CONTROLLER');
@@ -7,7 +8,7 @@ function CreationController (DatabaseConntection,ExpressApplication){
 
     this.app.use('/database/create',(req,res) => {
 		//Create a table that contains different access levels for each user, i.e admins etc.
-        this.connection.query('CREATE TABLE IF NOT EXISTS users(_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,username VARCHAR(55) NOT NULL,password VARCHAR(550) NOT NULL,email VARCHAR(55) NOT NULL,logged_in BOOL NOT NULL DEFAULT false,firstname VARCHAR(99),lastname VARCHAR(99),joined TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,profile VARCHAR(999),notif_token VARCHAR(255))',(err,result,fields) => {
+        this.connection.query('CREATE TABLE IF NOT EXISTS users(_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,username VARCHAR(55) NOT NULL,password VARCHAR(550) NOT NULL,email VARCHAR(55) NOT NULL,logged_in BOOL NOT NULL DEFAULT false,firstname VARCHAR(99),lastname VARCHAR(99),joined TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,profile VARCHAR(999),notif_token VARCHAR(255),api_key VARCHAR(255))',(err,result,fields) => {
 			if(!err) {
 				this.connection.query('CREATE TABLE IF NOT EXISTS shared(_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,sharer INT NOT NULL,title VARCHAR(100) NOT NULL,artist VARCHAR(100) NOT NULL,art VARCHAR(100) NOT NULL,time_shared TIMESTAMP DEFAULT CURRENT_TIMESTAMP,duration INT DEFAULT 0,spotify_id VARCHAR(999))',(err,result) => {
 					if(!err) {
@@ -72,7 +73,7 @@ function CreationController (DatabaseConntection,ExpressApplication){
     this.app.use('/database/seed',async (req,res) => {
 		await bcrypt.hash('drmario', 10,(err,hash) => {
 			if(!err) {
-				this.connection.query('INSERT INTO users(username,password,email,firstname,lastname,notif_token) VALUES("maxx730","' + hash + '","max.kinghorn@gmail.com","Max","Kinghorn",""),("tolm640","' + hash + '","tolm.lamar@gmail.com","Tom","Lamar",""),("ethan54","' + hash + '","ethan.clokels@gmail.com","Ethan","Clokey","");',(err,result) => {
+				this.connection.query('INSERT INTO users(username,password,email,firstname,lastname,notif_token,apikey) VALUES("maxx730","' + hash + '","max.kinghorn@gmail.com","Max","Kinghorn","","' + crypto.createHash('sha1') + '"),("tolm640","' + hash + '","tolm.lamar@gmail.com","Tom","Lamar","",""),("ethan54","' + hash + '","ethan.clokels@gmail.com","Ethan","Clokey","","");',(err,result) => {
 					if(!err) {
 						this.connection.query('INSERT INTO shared(sharer,title,artist,art,spotify_id) VALUES(2,"TEST 1","ARTIST 1","ART 1","SPOTIFY_ID_1"),(2,"TEST 2","ARTIST 2","ART 2","SPOTIFY_ID_2"),(1,"TEST 3","ARTIST 3","ART 3","SPOTIFY_ID_3"),(1,"TEST 4","ARTIST 4","ART 4","SPOTIFY_ID_4"),(3,"TEST 5","ARTIST 5","ART 5","SPOTIFY_ID_5"),(1,"TEST 6","ARTIST 6","ART 6","SPOTIFY_ID_6")',(err,result) => {
 							if(!err) {
